facbd2050dd6e9503b1548d8acf77c8ba772ea08
Akashdeep Dhar <49605954+t0xic0der@users.noreply.github.com>
2020-03-22 21:34:06 +0530

    Added configuration file
    Database, timestamp, version control and error messages

---
+++ libr_fgconfig.py
@@ -0,0 +1,95 @@
+dataunit = {
+    "localuse": {
+        "type": "SQLite3",
+        "path": "database/localuse.sqlt"
+    },
+    "clouduse": {
+        "type": "SQLite3",
+        "path": "database/clouduse.sqlt"
+    }
+}
+
+timedata = {
+    "weeklist": {
+        "0": {"tinyform": "Mon", "fullform": "Monday"},
+        "1": {"tinyform": "Tue", "fullform": "Tuesday"},
+        "2": {"tinyform": "Wed", "fullform": "Wednesday"},
+        "3": {"tinyform": "Thu", "fullform": "Thursday"},
+        "4": {"tinyform": "Fri", "fullform": "Friday"},
+        "5": {"tinyform": "Sat", "fullform": "Saturday"},
+        "6": {"tinyform": "Sun", "fullform": "Sunday"}
+    },
+    "montlist": {
+        "1": {
+            "tinyform": "Jan",
+            "fullform": "January"
+        },
+        "2": {
+            "tinyform": "Feb",
+            "fullform": "February"
+        },
+        "3": {
+            "tinyform": "Mar",
+            "fullform": "March"
+        },
+        "4": {
+            "tinyform": "Apr",
+            "fullform": "April"
+        },
+        "5": {
+            "tinyform": "May",
+            "fullform": "May"
+        },
+        "6": {
+            "tinyform": "Jun",
+            "fullform": "June"
+        },
+        "7": {
+            "tinyform": "Jul",
+            "fullform": "July"
+        },
+        "8": {
+            "tinyform": "Aug",
+            "fullform": "August"
+        },
+        "9": {
+            "tinyform": "Sep",
+            "fullform": "September"
+        },
+        "10": {
+            "tinyform": "Oct",
+            "fullform": "October"
+        },
+        "11": {
+            "tinyform": "Nov",
+            "fullform": "November"
+        },
+        "12": {
+            "tinyform": "Dec",
+            "fullform": "December"
+        }
+    }
+}
+
+versinfo = {
+    "mainline": "0.0.1b",
+    "warnmail": "This protocol is not production ready"
+}
+
+erorlist = {
+    "sucesful": "Your account has been created.",
+    "uneqpass": "The passwords provided do not match. Try again.",
+    "nameabst": "The fullname field is mandatory.",
+    "usnmabst": "The username field is mandatory.",
+    "passabst": "The password field is mandatory.",
+    "reptabst": "The repeat password field is mandatory.",
+    "mailabst": "The email address field is mandatory.",
+    "userexst": "An account with this username already exists.",
+    "noscuser": "No account with such username is registered.",
+    "wrngpswd": "The password entered is wrong. Try again.",
+    "norecvsp": "You did not specify a receiver.",
+    "nosubjsp": "You did not specify a directive subject.",
+    "contemty": "The directive cannot be sent empty.",
+    "recvabst": "No receiver with the provided username exists.",
+    "mailsucc": "Your directive has been sent successfully"
+}
